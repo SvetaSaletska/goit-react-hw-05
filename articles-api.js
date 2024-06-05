e96a220a67f8b5af5575989885068d5eb0ad0e9d
SvetaSaletska <svitlanagalevich@gmail.com>
2024-06-06 00:29:37 +0300

    update

--- articles-api.js
+++ articles-api.js
@@ -1,22 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://api.unsplash.com";
-
-// export const fetchImages = async (query, page) => {
-//   console.log(query);
-//   const response = await axios.get("/search/photos", {
-//     params: {
-//       client_id: "czbUvZULGNNC7UsIcW08Kp2aJWei_H3EibNIqCy4xjg",
-//       query,
-//       page,
-//       hitsPerPage: 12,
-//       orientation: "landscape",
-//     },
-//   });
-//   return response.data.results;
-// };
-
-const baseURL = "https://api.themoviedb.org/3";
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
 // "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
 
@@ -28,23 +12,35 @@ const options = {
   },
 };
 
-axios
-  .get(baseURL, options)
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+export const getMovies = async () => {
+  const response = await axios.get("/trending/movie/day", options);
+  return response.data;
+};
 
-// axios.defaults.baseURL = "https://api.unsplash.com";
+export const getSearchMovies = async (query) => {
+  const response = await axios.get("/search/movie", {
+    params: {
+      query: query,
+    },
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZjI2MTgwYjRiNDBlN2JlYjUwOWEyM2RjYjgyNjQyOSIsInN1YiI6IjY2NWY1YTJmMjY1MWUyNmE2NDA5MDMzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XAEjrNFGiMTGl-AMjGVub1ObI7GCLn60c8HA8VYASyI",
+    },
+  });
+  return response.data.results;
+};
 
-// export const fetchImages = async (query, page) => {
-//   console.log(query);
-//   const response = await axios.get("/search/photos", {
-//     params: {
-//       client_id: "czbUvZULGNNC7UsIcW08Kp2aJWei_H3EibNIqCy4xjg",
-//       query,
-//       page,
-//       hitsPerPage: 12,
-//       orientation: "landscape",
-//     },
-//   });
-//   return response.data.results;
-// };
+export const getDetails = async (movieId) => {
+  const response = await axios.get(`/movie/${movieId}`, options);
+  return response.data;
+};
+
+export const getCastMovie = async (movieId) => {
+  const response = await axios.get(`/movie/${movieId}/credits`, options);
+  return response.data.cast;
+};
+
+export const getReviewsMovie = async (movieId) => {
+  const response = await axios.get(`/movie/${movieId}/reviews`, options);
+  return response.data.results;
+};
